e24ed8ebb7d9c4443bf957f41ff99a8c4939f055
Dohyeong Kim <kimbring2@gmail.com>
2023-01-10 21:57:13 +0900

    Add files via upload
    Reduce the maximum episode step from 4500 to 1000

--- actor.py
+++ actor.py
@@ -21,7 +21,7 @@ arguments = parser.parse_args()
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
-writer = tf.summary.create_file_writer("tensorboard")
+writer = tf.summary.create_file_writer("tensorboard_kill")
 
 context = zmq.Context()
 
@@ -32,7 +32,9 @@ socket.connect("tcp://localhost:" + str(5555 + arguments.env_id))
 
 # Create a new environment object.
 env = deepmind_lab.Lab("ctf_simple", ['RGB_INTERLEAVED'],
-                       {'fps': '30', 'width': '80', 'height': '60'})
+                       {'fps': '30', 'width': '640', 'height': '640'})
+#env = deepmind_lab.Lab("contributed/dmlab30/explore_goal_locations_small", ['RGB_INTERLEAVED'],
+#                       {'fps': '60', 'width': '640', 'height': '640'})
 
 def _action(*entries):
   return np.array(entries, dtype=np.intc)
@@ -40,23 +42,19 @@ def _action(*entries):
 ACTIONS = {
       'look_left': _action(-20, 0, 0, 0, 0, 0, 0),
       'look_right': _action(20, 0, 0, 0, 0, 0, 0),
-      'look_up': _action(0, 10, 0, 0, 0, 0, 0),
-      'look_down': _action(0, -10, 0, 0, 0, 0, 0),
       'strafe_left': _action(0, 0, -1, 0, 0, 0, 0),
       'strafe_right': _action(0, 0, 1, 0, 0, 0, 0),
       'forward': _action(0, 0, 0, 1, 0, 0, 0),
       'backward': _action(0, 0, 0, -1, 0, 0, 0),
       'fire': _action(0, 0, 0, 0, 1, 0, 0),
-      'jump': _action(0, 0, 0, 0, 0, 1, 0),
-      'crouch': _action(0, 0, 0, 0, 0, 0, 1)
   }
 
 
 ACTION_LIST = list(ACTIONS)
 #print("ACTION_LIST: ", ACTION_LIST)
 
-num_actions = 11
-state_size = (80,80,3)
+num_actions = 7
+state_size = (84,84,3)
 
 
 def render(obs):
@@ -70,10 +68,10 @@ average = []
 for episode_step in range(0, 2000000):
     env.reset(seed=1)
     obs = env.observations()
-    obs = cv2.cvtColor(obs['RGB_INTERLEAVED'], cv2.COLOR_BGR2RGB)
+    obs_raw = cv2.cvtColor(obs['RGB_INTERLEAVED'], cv2.COLOR_BGR2RGB)
+    obs = cv2.resize(obs_raw, dsize=(84,84), interpolation=cv2.INTER_AREA)
 
-    obs_resized = cv2.resize(obs, dsize=(80,80), interpolation=cv2.INTER_AREA)
-    state = obs_resized / 255.0
+    state = obs / 255.0
 
     done = False
     reward = 0.0
@@ -81,8 +79,6 @@ for episode_step in range(0, 2000000):
     step = 0
     while True:
         try:
-            step += 1
-
             state_reshaped = np.reshape(state, (1,*state_size)) 
 
             env_output = {"env_id": np.array([arguments.env_id]), 
@@ -91,29 +87,21 @@ for episode_step in range(0, 2000000):
                           "observation": state_reshaped}
             socket.send_pyobj(env_output)
             action = int(socket.recv_pyobj()['action'])
-            
-            #obs1, reward, done, _ = env.step(ACTION_LIST[action])
-            reward = env.step(ACTIONS[ACTION_LIST[action]])
-            obs1 = env.observations()
+
+            reward = env.step(ACTIONS[ACTION_LIST[action]], num_steps=2)
+            #reward = reward
+            #if reward != 2.0:
+                #print("flag_reward")
+            #    reward = 0.0
+
             done = not env.is_running()
-
-            obs1 = cv2.cvtColor(obs1['RGB_INTERLEAVED'], cv2.COLOR_BGR2RGB)
-            obs1_resized = cv2.resize(obs1, dsize=(80,80), interpolation=cv2.INTER_AREA)
-            next_state = obs1_resized / 255.0
-
-            if arguments.env_id == 0: 
-                render(obs1_resized)
-
-            reward_sum += reward
-            state = next_state
-            if done:
+            if done or step == 1000:
                 if arguments.env_id == 0:
                     scores.append(reward_sum)
                     episodes.append(episode_step)
                     average.append(sum(scores[-50:]) / len(scores[-50:]))
 
                     with writer.as_default():
-                        #print("average[-1]: ", average[-1])
                         tf.summary.scalar("average_reward", average[-1], step=episode_step)
                         writer.flush()
 
@@ -123,7 +111,20 @@ for episode_step in range(0, 2000000):
 
                 break
 
+            obs1 = env.observations()
+            obs1_raw = cv2.cvtColor(obs1['RGB_INTERLEAVED'], cv2.COLOR_BGR2RGB)
+            obs1 = cv2.resize(obs1_raw, dsize=(84,84), interpolation=cv2.INTER_AREA)
+            next_state = obs1 / 255.0
+
+            if arguments.env_id == 0: 
+                render(obs1_raw)
+
+            reward_sum += reward
+            state = next_state
+
+            step += 1
         except (tf.errors.UnavailableError, tf.errors.CancelledError):
+            print('Inference call failed. This is normal at the end of training.')
             logging.info('Inference call failed. This is normal at the end of training.')
 
 env.close()
