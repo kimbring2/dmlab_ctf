d6c9d9004eaed965bdd40ff15fea7adccb4e179e
Dohyeong Kim <kimbring2@gmail.com>
2023-09-12 19:00:35 +0900

    Add files via upload

---
+++ map_copy_dmlab.sh
@@ -0,0 +1,6 @@
+DMLAB_PATH=$1
+
+cp ctf_simple.aas "${DMLAB_PATH}/assets/maps/built"
+cp ctf_simple.bsp "${DMLAB_PATH}/assets/maps/built"
+cp ctf_simple.lua "${DMLAB_PATH}/game_scripts/levels"
+cp ctf_simple_factory.lua "${DMLAB_PATH}/game_scripts/factories"
---
+++ map_copy_python.sh
@@ -0,0 +1,6 @@
+DMLAB_PATH=$1
+
+sudo cp ctf_simple.aas "${DMLAB_PATH}/baselab/maps"
+sudo cp ctf_simple.bsp "${DMLAB_PATH}/baselab/maps"
+sudo cp ctf_simple.lua "${DMLAB_PATH}/baselab/game_scripts/levels"
+sudo cp ctf_simple_factory.lua "${DMLAB_PATH}/baselab/game_scripts/factories"
