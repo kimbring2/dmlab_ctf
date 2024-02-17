b7a0594c39dc29fc3fb5cfe58a697a36edb6f179
Dohyeong Kim <kimbring2@gmail.com>
2024-02-18 06:31:16 +0900

    Add files via upload

--- map_copy_dmlab.sh
+++ map_copy_dmlab.sh
@@ -1,6 +1,11 @@
 DMLAB_PATH=$1
 
-cp ctf_simple.aas "${DMLAB_PATH}/assets/maps/built"
-cp ctf_simple.bsp "${DMLAB_PATH}/assets/maps/built"
-cp ctf_simple.lua "${DMLAB_PATH}/game_scripts/levels"
-cp ctf_simple_factory.lua "${DMLAB_PATH}/game_scripts/factories"
+cp maps/ctf_simple.aas "${DMLAB_PATH}/assets/maps/built"
+cp maps/ctf_simple.bsp "${DMLAB_PATH}/assets/maps/built"
+cp maps/ctf_simple.lua "${DMLAB_PATH}/game_scripts/levels"
+cp maps/ctf_simple_factory.lua "${DMLAB_PATH}/game_scripts/factories"
+
+cp maps/ctf_middle.aas "${DMLAB_PATH}/assets/maps/built"
+cp maps/ctf_middle.bsp "${DMLAB_PATH}/assets/maps/built"
+cp maps/ctf_middle.lua "${DMLAB_PATH}/game_scripts/levels"
+cp maps/ctf_middle_factory.lua "${DMLAB_PATH}/game_scripts/factories"
--- map_copy_python.sh
+++ map_copy_python.sh
@@ -1,6 +1,11 @@
 DMLAB_PATH=$1
 
-sudo cp ctf_simple.aas "${DMLAB_PATH}/baselab/maps"
-sudo cp ctf_simple.bsp "${DMLAB_PATH}/baselab/maps"
-sudo cp ctf_simple.lua "${DMLAB_PATH}/baselab/game_scripts/levels"
-sudo cp ctf_simple_factory.lua "${DMLAB_PATH}/baselab/game_scripts/factories"
+sudo cp maps/ctf_simple.aas "${DMLAB_PATH}/baselab/maps"
+sudo cp maps/ctf_simple.bsp "${DMLAB_PATH}/baselab/maps"
+sudo cp maps/ctf_simple.lua "${DMLAB_PATH}/baselab/game_scripts/levels"
+sudo cp maps/ctf_simple_factory.lua "${DMLAB_PATH}/baselab/game_scripts/factories"
+
+sudo cp maps/ctf_middle.aas "${DMLAB_PATH}/baselab/maps"
+sudo cp maps/ctf_middle.bsp "${DMLAB_PATH}/baselab/maps"
+sudo cp maps/ctf_middle.lua "${DMLAB_PATH}/baselab/game_scripts/levels"
+sudo cp maps/ctf_middle_factory.lua "${DMLAB_PATH}/baselab/game_scripts/factories"
